0a46a6516cb35f13e2d64b691f1f00134ee2bf3b
Dan Ramalho <Dramalho7@gmail.com>
2017-08-29 10:53:51 -0400

    migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "teacher#index"
+
+root to: "teachers#index"
+resources :teachers
+resources :students
+resources :courses
+resources :behavior_reports
+resources :participation_reports
+resources :grades
+resources :parents
+resources :attendances
+
 end
