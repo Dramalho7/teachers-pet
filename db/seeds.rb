aef77336b81a4c1ce2604ebb612f6dfae4234d4c
Dan Ramalho <Dramalho7@gmail.com>
2017-08-29 13:51:33 -0400

    seeded 2 teachers and some classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# teacher = Teacher.create(email: 'dramalho7@gmail.com', password: 'Nycda1', password_confirmation: 'Nycda1', name: 'Mr. Ramalho', avatar:'assets/mr.png')
+# teacher = Teacher.create(email: 'mip12793@gmail.com', password: 'Nycda1', password_confirmation: 'Nycda1', name: 'Mr. Keating', avatar:'assets/keating.png')
+
+# student = Student.create(name: 'Dan Ramalho', parent_email:'dramalho7@gmail.com', avatar: 'assets/little.png')
+# student = Student.create(name: 'Malik Pervez', parent_email:'mip12793@gmail.com', avatar: 'assets/malik.png')
+# student = Student.create(name: 'Randall Song', parent_email:'randallsong149@gmail.com', avatar: 'assets/randall.png')
+# student = Student.create(name: 'Davey Patlut', parent_email:'dpatlut@gmail.com', avatar: 'assets/david.png')
+
+# student = Student.create(name: 'Ro Rudra', parent_email:'rohinir315@gmail.com', avatar: 'assets/ro.png')
+# student = Student.create(name: 'Wesley Stuckmeyer', parent_email:'wstuckmeyer@gmail.com', avatar: 'assets/wesley.png')
+# student = Student.create(name: 'Richy Love', parent_email:'ricklove@outlook.com', avatar: 'assets/rick.png')
+# student = Student.create(name: 'Leighton Lee', parent_email:'leightonlee99@gmail.com', avatar: 'assets/leighton.png')
+
+# student = Student.create(name: 'Britney Spears', parent_email:'test@gmail.com', avatar: 'assets/britney.png')
+# student = Student.create(name: 'Arya Stark', parent_email:'test@gmail.com', avatar: 'assets/arya.png')
+# student = Student.create(name: 'Sansa Stark', parent_email:'test@gmail.com', avatar: 'assets/sansa.png')
+# student = Student.create(name: 'Bran Stark', parent_email:'test@gmail.com', avatar: 'assets/bran.png')
+
+# student = Student.create(name: 'Geoffrey Barratheon', parent_email:'test@gmail.com', avatar: 'assets/geoffrey.png')
+# student = Student.create(name: 'Rickon Stark', parent_email:'test@gmail.com', avatar: 'assets/rickon.png')
+# student = Student.create(name: 'Theon Grayjoy', parent_email:'test@gmail.com', avatar: 'assets/theon.png')
+
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '2', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '3', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '4', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '5', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '6', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '7', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '8', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '9', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '10', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '11', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '12', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '13', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '14', avatar: 'assets/cell.png')
+# course = Course.create(name: 'Period 1 Bio', teacher_id: '1', student_id: '15', avatar: 'assets/cell.png')
+
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '1', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '2', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '3', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '4', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '5', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '6', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '7', avatar: 'assets/english.png')
+# course = Course.create(name: 'Period 4 English', teacher_id: '2', student_id: '8', avatar: 'assets/english.png')
 
 
-student = Student.create(name: 'Dan Ramalho', parent_email:'dramalho7@gmail.com', avatar: 'assets/little.png')
-student = Student.create(name: 'Malik Pervez', parent_email:'mip12793@gmail.com', avatar: 'assets/malik.png')
-student = Student.create(name: 'Randall Song', parent_email:'randallsong149@gmail.com', avatar: 'assets/randall.png')
-student = Student.create(name: 'Davey Patlut', parent_email:'dpatlut@gmail.com', avatar: 'assets/david.png')
 
-student = Student.create(name: 'Ro Rudra', parent_email:'rohinir315@gmail.com', avatar: 'assets/ro.png')
-student = Student.create(name: 'Wesley Stuckmeyer', parent_email:'wstuckmeyer@gmail.com', avatar: 'assets/wesley.png')
-student = Student.create(name: 'Richy Love', parent_email:'ricklove@outlook.com', avatar: 'assets/rick.png')
-student = Student.create(name: 'Leighton Lee', parent_email:'leightonlee99@gmail.com', avatar: 'assets/leighton.png')
 
-student = Student.create(name: 'Britney Spears', parent_email:'test@gmail.com', avatar: 'assets/britney.png')
-student = Student.create(name: 'Arya Stark', parent_email:'test@gmail.com', avatar: 'assets/arya.png')
-student = Student.create(name: 'Sansa Stark', parent_email:'test@gmail.com', avatar: 'assets/sansa.png')
-student = Student.create(name: 'Bran Stark', parent_email:'test@gmail.com', avatar: 'assets/bran.png')
 
-student = Student.create(name: 'Geoffrey Barratheon', parent_email:'test@gmail.com', avatar: 'assets/geoffrey.png')
-student = Student.create(name: 'Rickon Stark', parent_email:'test@gmail.com', avatar: 'assets/rickon.png')
-student = Student.create(name: 'Theon Grayjoy', parent_email:'test@gmail.com', avatar: 'assets/theon.png')
+
