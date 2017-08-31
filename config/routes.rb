eaf3bbc0a2abd429fb1ad6e825349745a27de29b
Malik <mip12793@gmail.com>
2017-08-31 10:20:19 -0400

    fixed minor issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root to: "teachers#index"
-resources :teachers
+resources :teachers, 
 resources :students
 resources :courses
 resources :behavior_reports
