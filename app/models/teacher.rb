db005a7b2a034d667c42b377e208924ae011d268
Dan Ramalho <Dramalho7@gmail.com>
2017-08-31 10:20:04 -0400

    edit avatar form

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,13 +1,11 @@
 class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
   mount_uploader :avatar, AvatarUploader
-   validates_integrity_of  :avatar
-  validates_processing_of :avatar
   has_many :courses
   has_many :students, :through => :courses
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
- 
+  
 end
