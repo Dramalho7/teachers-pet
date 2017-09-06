ed44aba30d0343e365bff211b4e3e0b79e13b6ab
Dan Ramalho <Dramalho7@gmail.com>
2017-09-05 21:19:37 -0400

    parent controller changes email, and index page

--- app/controllers/behavior_reports_controller.rb
+++ app/controllers/behavior_reports_controller.rb
@@ -10,17 +10,6 @@ class BehaviorReportsController < ApplicationController
 		@behaviorReport = BehaviorReport.create(date: params[:behavior_report][:date], report: params[:behavior_report][:report], student_id: params[:behavior_report][:student_id])
 		@student = Student.find(params[:behavior_report][:student_id])
 		@teacher = current_teacher.name
-		from = Email.new(email: 'test@example.com')
-		to = Email.new(email: 'dramalho7@gmail.com')
-		subject = 'Guardian of' + ' ' + @student.name 
-		content = Content.new(type: 'text/plain', value: 'There has been an incident at school today' + ' ' + @behaviorReport.report + ' ' + 'if you have any questions, please let me know. Sincerely,' + ' ' + @teacher)
-		mail = SendGrid::Mail.new(from, subject, to, content)
-
-		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-		response = sg.client.mail._('send').post(request_body: mail.to_json)
-		puts response.status_code
-		puts response.body
-		puts response.headers
 		redirect_to student_path(@student.id)
 	end
 
